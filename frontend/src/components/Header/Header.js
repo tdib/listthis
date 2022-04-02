eda8af7de3e4e341f0b6014688f9a21b93f17c7f
Thomas Dib <s3838765@student.rmit.edu.au>
2022-04-03 00:01:55 +1100

    Fix import for DayJS advanced format

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Title, Subtitle, HRule } from './headerStyle'
 import dayjs from 'dayjs'
-import * as advancedFormat from 'dayjs/plugin/advancedFormat'
+import advancedFormat from 'dayjs/plugin/advancedFormat'
 
 const Header = () => {
   dayjs.extend(advancedFormat)
