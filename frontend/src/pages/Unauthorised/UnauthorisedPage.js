6e0268d6d2f419de872dd4e7673cbbb628450545
Thomas Dib <s3838765@student.rmit.edu.au>
2022-06-30 19:39:47 +1000

    Nuke project

--- frontend/src/pages/Unauthorised/UnauthorisedPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const UnauthorisedPage = () => {
-  return <h1>UNAUTHORISED TO VIEW THIS PAGE</h1>
-}
-
-export default UnauthorisedPage
--- frontend/src/stores/index.js
+++
@@ -1,3 +0,0 @@
-export { default as useListStore } from './useListStore'
-export { default as useListsStore } from './useListsStore'
-export { default as useUserStore } from './useUserStore'
