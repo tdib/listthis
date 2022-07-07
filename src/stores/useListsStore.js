c6e4d2fcf3f07a4347f2a6534cf8c010f75ef0ff
Thomas Dib <s3838765@student.rmit.edu.au>
2022-07-07 20:47:18 +1000

    Persist lists store

--- src/stores/useListsStore.js
+++ src/stores/useListsStore.js
@@ -1,31 +1,37 @@
 import create from 'zustand'
+import { persist } from 'zustand/middleware'
 
-const useListsStore = create(set => ({
-  lists: null,
+const useListsStore = create(
+  persist(
+    set => ({
+      lists: null,
 
-  loadLists: lists => set({ lists: lists }),
+      loadLists: lists => set({ lists: lists }),
 
-  unloadLists: () => set({ lists: null }),
+      unloadLists: () => set({ lists: null }),
 
-  leaveList: listToRemoveID =>
-    set(state => ({
-      lists: state.lists.filter(list => list.listUID !== listToRemoveID),
-    })),
+      leaveList: listToRemoveID =>
+        set(state => ({
+          lists: state.lists.filter(list => list.listUID !== listToRemoveID),
+        })),
 
-  createList: list =>
-    set(state => ({
-      lists: [...state.lists, list],
-    })),
+      createList: list =>
+        set(state => ({
+          lists: [...state.lists, list],
+        })),
 
-  // TODO: update list
-  upsertList: newList => {
-    set(state => ({
-      lists: state.lists.find(list => list.listUID === newList.listUID)
-      ? state.lists.map(list => list.listUID === newList.listUID ? newList : list)
-      : [...state.lists, newList]
-      // lists: state.lists.map(list => list.listUID === newList.listUID ? newList : list)
-    }))
-  }
-}))
+      // TODO: update list
+      upsertList: newList => {
+        set(state => ({
+          lists: state.lists.find(list => list.listUID === newList.listUID)
+          ? state.lists.map(list => list.listUID === newList.listUID ? newList : list)
+          : [...state.lists, newList]
+          // lists: state.lists.map(list => list.listUID === newList.listUID ? newList : list)
+        }))
+      }
+    }),
+    { name: 'listsCache' }
+  )
+)
 
 export default useListsStore
